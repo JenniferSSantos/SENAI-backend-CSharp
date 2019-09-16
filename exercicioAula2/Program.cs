c733de51c62306c6c40d794de46edf7cd41828d9
FIC <FIC@info.local>
2019-09-16 15:34:33 -0300

    Exercicio 1 da Aula 2-3

---
+++ exercicioAula2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace exercicioAula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             double salario, s1, d1;
+            double resultado;
+
+            Console.Write("Digite seu salário");
+            salario = Double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor da divida");
+            d1 = Double.Parse(Console.ReadLine());
+
+            
+            if(salario > d1){
+                Console.WriteLine("Positivo");
+            }
+            else{
+                Console.WriteLine("Negativo");
+            }
+        }
+    }
+}
