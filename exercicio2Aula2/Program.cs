2d7df236913841ce3b041b045ce0baba58dbc9b8
FIC <FIC@info.local>
2019-09-16 15:54:53 -0300

    Exercicio 2 aula 2

---
+++ exercicio2Aula2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercicio2Aula2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double precototal;
+
+            Console.Write("Digite o preço total:  ");
+            precototal = Double.Parse(Console.ReadLine());
+
+            if(precototal <= 100){
+                precototal = precototal - 25;
+            }
+
+            Console.Write(precototal);
+
+
+        }
+    }
+}
