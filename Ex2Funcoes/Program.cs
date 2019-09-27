0917104c82a5d995bedbdff40cb3296b8232ace9
FIC <FIC@info.local>
2019-09-27 11:07:02 -0300

    Exercicio 2 funções

---
+++ Ex2Funcoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Ex2Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            diadasemana();
+        }
+        static void diadasemana (){
+         DateTime diaatual = DateTime.Today;
+
+           
+            Console.WriteLine(diaatual.DayOfWeek);
+
+
+        
+
+
+            
+
+       }
+    }
+}
